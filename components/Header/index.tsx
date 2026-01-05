9c81664d7d1fcb8e8765de83b8fb32438860b17a
Ricardo <ricardoandersonjunior@outlook.com>
2026-01-05 20:31:43 -0300

    headerFront

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 export default function Header() {
-
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <img src='/logo.png' alt="Logo" className={styles.logo} />
+        <img src="/logo.png" alt="Logo" className={styles.logo} />
+
         <div className={styles.menuLinks}>
+          
+          <div className={styles.searchBox}>
+            <input type="text" placeholder="Buscar curso..." />
+            <button type="submit">🔍</button>
+          </div>
+
           <a href="#" className={styles.menuLink}>UX/UI</a>
           <a href="#" className={styles.menuLink}>React</a>
           <a href="#" className={styles.menuLink}>Next</a>
